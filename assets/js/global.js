1983b40430a0d0265dacd08c3a1815d988606f8d
Thomas Trusch <t.trusch@wellcome.ac.uk>
2016-09-29 11:54:01 +0100

    Remove unused files - Add assets structure and files

---
+++ assets/js/global.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    // Selects href from images without alt text
+  function altTextChecker() {
+
+    $('img').each(function() {
+      if($(this).attr('alt') == "" || $(this).attr('alt') === undefined) {
+        console.log($(this).attr('href'));
+      }
+    });
+  };
+
+  altTextChecker();
+});
